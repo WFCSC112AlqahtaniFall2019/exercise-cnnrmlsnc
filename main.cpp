d18edf0e7b2b162f012491d9449601a18154591a
conma <milecw17@wfu.edu>
2019-09-05 16:24:26 -0400

    hi :) this is connor mileusnics work

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
     int revVctr[8];               // User values
     int i;                        // Loop index
+    vector<int> hold(8);
+
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
@@ -13,14 +17,23 @@ int main() {
 
     // Reverse
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+        hold[i] = revVctr[NUM_ELEMENTS - 1 - i];
     }
+
+    // Swap
+    for (i = 0; i < NUM_ELEMENTS; ++i) {
+        revVctr[i] = hold[i];
+    }
+
+
 
     // Print values
     cout << endl << "New values: ";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
         cout << " " << revVctr[i];
     }
+
+
     cout << endl;
 
     return 0;
